27066c1e45edc670611be2903c09f30ddfb83272
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2019-06-20 10:49:16 -0400

    Use more robust method for getting absolute path
    Previous version would fail when script was `source`d.

--- scripts/get-source-directory.bash
+++ scripts/get-source-directory.bash
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
 
 # Get the source directory of this script as an absolute path.
-DIR=$(dirname "$(readlink -f "$0")")
+DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 echo $DIR
